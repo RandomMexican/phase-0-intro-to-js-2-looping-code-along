1f3bb619698209c5d112728654b1e9d9bbd69e2e
mariovaquero <mvaquero143@gmail.com>
2023-08-24 18:13:40 -0400

    Complete Loop Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const { list } = require("mocha/lib/reporters/base");
+
 // Code your solutions in this file
+const names = [`Guadalupe`,`Ollie`,`Aki`];
+
+function writeCards(names, event)
+{
+    const newlist = []
+    for (let i = 0; i < names.length; i++)
+    {
+        newlist.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        
+    }
+    return newlist;
+    
+}
+
+function countDown(number)
+{
+    while(number >=0)
+    {
+        console.log(number--);
+    }
+}
+writeCards([`Guadalupe`,`Ollie`,`Aki`],`surprise`);
+//countDown(10);
